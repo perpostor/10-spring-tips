6cfa01511c88f539ff49d6586eae40fb4cb7e795
veselin-vasilev <false.breakfast@gmail.com>
2022-08-07 20:42:14 +0100

    Remove audit logic

--- src/main/java/demo/quotation/QuotationService.java
+++ src/main/java/demo/quotation/QuotationService.java
@@ -1,32 +1,24 @@
 package demo.quotation;
 
 import demo.aspect.LogExecutionTime;
-import demo.quotation.model.QuotationEvent;
 import demo.quotation.ref.model.Quote;
 import demo.quotation.strategy.MarkupTemplate;
 import demo.quotation.strategy.Strategy;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisherAware;
-import org.springframework.stereotype.Service;
-
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class QuotationService implements ApplicationEventPublisherAware {
+public class QuotationService {
 
-  private ApplicationEventPublisher eventPublisher;
   private final Map<String, MarkupTemplate> markupStrategies;
 
   @LogExecutionTime
   public Quote getQuote(final String clientId, final String ccyPair) {
     var strategy = determineStrategy(clientId);
-    Quote rate = markupStrategies.get(strategy.toString()).markupQuote(ccyPair);
 
-    eventPublisher.publishEvent(new QuotationEvent(this, rate, clientId, strategy));
-
-    return rate;
+    return markupStrategies.get(strategy.toString()).markupQuote(ccyPair);
   }
 
   private Strategy determineStrategy(final String clientId) {
@@ -37,8 +29,4 @@ public class QuotationService implements ApplicationEventPublisherAware {
     };
   }
 
-  @Override
-  public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-    eventPublisher = applicationEventPublisher;
-  }
 }
